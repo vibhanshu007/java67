95f1ffde6d97cc0abd9e56b39ce511069ca6cd8d
tabkidsadmin <vibhanshumail849@gmail.com>
2016-11-30 17:16:12 +0530

    Reverse String method 1

---
+++ java67/src/com/vibs/java67/string/ReverseString.java
@@ -0,0 +1,11 @@
+package com.vibs.java67.string;
+
+public class ReverseString {
+	public static void main(String[] args) {
+		String myString = "VibsBro";
+		// 1st method 
+		StringBuffer sb = new StringBuffer(myString);
+		System.out.println(sb.reverse());
+		
+	}
+}
