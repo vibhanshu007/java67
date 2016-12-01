9444cffb0979235e299a89079304b7367c9159ae
vibs <vibhanshumail849@gmail.com>
2016-12-01 18:01:27 +0530

    Pattern.. Done

--- java67/src/com/vibs/conceptOfDay/pattern/NumberTringle.java
+++ java67/src/com/vibs/conceptOfDay/pattern/NumberTringle.java
@@ -8,8 +8,31 @@ public class NumberTringle {
 		
 		Scanner sn = new Scanner(System.in);
 		System.out.print("Enter the no of row - ");
-		int noOfRow = sn.nextInt();
+		int noOfRows = sn.nextInt();
 		int rowCount = 1;
+		 for (int i = noOfRows; i > 0; i--)
+	        {
+	            //Printing i spaces at the beginning of each row
+	 
+	            for (int j = 1; j <= i; j++)
+	            {
+	                System.out.print(" ");
+	            }
+	 
+	            //Printing 'rowCount' value 'rowCount' times at the end of each row
+	 
+	            for (int j = 1; j <= rowCount; j++)
+	            {
+	                System.out.print(rowCount+" ");
+	            }
+	 
+	            System.out.println();
+	 
+	            //Incrementing the rowCount
+	 
+	            rowCount++;
+	        }
+	    }
 	}
 
-}
+
