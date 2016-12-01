05f64c496d05137d6a91bb5fd8c3e26acfe76b9f
vibs <vibhanshumail849@gmail.com>
2016-12-01 17:55:59 +0530

    pattern Program

---
+++ java67/src/com/vibs/conceptOfDay/pattern/NumberTringle.java
@@ -0,0 +1,14 @@
+package com.vibs.conceptOfDay.pattern;
+
+import java.util.Scanner;
+
+public class NumberTringle {
+	
+	public static void main(String[] args) {
+		
+		Scanner sn = new Scanner(System.in);
+		System.out.print("Enter the no of row - ");
+		int noOfRow = sn.nextInt(); 
+	}
+
+}
--- java67/src/com/vibs/java67/string/ReverseString.java
+++ java67/src/com/vibs/java67/string/ReverseString.java
@@ -3,9 +3,31 @@ package com.vibs.java67.string;
 public class ReverseString {
 	public static void main(String[] args) {
 		String myString = "VibsBro";
-		// 1st method 
+		
+		// 1st method
+		
 		StringBuffer sb = new StringBuffer(myString);
 		System.out.println(sb.reverse());
 		
+		// 2nd method Iterative method
+		
+		char[] strArray  = myString.toCharArray();
+		for (int i = strArray.length-1; i >=0 ; i--) {
+			System.out.print(strArray[i]);
+		}
+		
+		// 3rd method
+		System.out.println(recursiveMethod(myString));
+		
 	}
+	static String recursiveMethod(String str)
+	{
+		if ((null == str) || (str.length() <= 1))
+		{
+			return str;
+		}
+		
+		return recursiveMethod(str.substring(1)) + str.charAt(0);
+	}
+	
 }
